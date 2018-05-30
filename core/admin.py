1dc1dcd73a6c7e83f768a80497d5ad4c0a14e0eb
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2018-05-30 10:30:51 -0300

    admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Ensino, Serie, Organizacao, Componente, Instituicao
 
 # Register your models here.
+
+
+admin.site.register(Ensino)
+admin.site.register(Serie)
+admin.site.register(Organizacao)
+admin.site.register(Componente)
+admin.site.register(Instituicao)
--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Ensino(models.Model):
-    nome = models.CharField(max_length=100, help_text="A etapa do ensino na educação em que a série está inserida, ex: Ensino Médio, Fundamental, EJA")
+    nome = models.CharField(max_length=100, help_text="A etapa do ensino na educação em que a série está inserida, ex: Infantil, Fundamental, Médio, EJA")
 
 class Serie(models.Model):
     ensino = models.ForeignKey(Ensino, on_delete=models.CASCADE, help_text="A etapa do ensino em que a série está inserida")
