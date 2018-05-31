5e9e612e2721b170d770b73a36df4b92c94bed6f
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2018-05-31 12:12:09 -0300

    esboćo de linha de testes

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Criar os diários de uma escola
+
+# Designar professores para as turmas
+
+# Atribuir os horários de aulas
+
+# Matricular alunos
+
+# Quais professores posso designar para aonde?
+
+# Quais as foramćões dos professores?
+
+# IMPLANTAĆÃO
+
+# Importar instituićões da organizaćão
+
+# Cradastrar matriz curricular com carga horárias para os componentes
